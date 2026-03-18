bba44608f3d9d1fcd7f7e74bf178f5fd24e411de
joshiawesome <dfelips@gmail.com>
2026-03-18 16:41:16 +0530

    mail setup

--- photos_store_app/app/mailers/user_mailer.rb
+++ photos_store_app/app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to PhotoStore!')
+    mail(to: @user.email, subject: 'Welcome!')
   end
 end
--- photos_store_app/test/mailers/user_mailer_test.rb
+++ photos_store_app/test/mailers/user_mailer_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome_email" do
+    user = User.new(email: "turtle@example.com", password: "123")
+
+    email = UserMailer.welcome_email(user)
+
+
+    assert_equal ["turtle@example.com"], email.to
+    assert_equal ["dfelips@gmail.com"], email.from
+    assert_equal "Welcome!", email.subject
+
+    assert_match "Thanks for signing up", email.body.encoded
+  end
 end
