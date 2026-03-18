69a4dca65a7a626712add6ed41610efebaf3c24f
joshiawesome <dfelips@gmail.com>
2026-03-18 16:29:22 +0530

    mail setup

--- photos_store_app/app/controllers/users_controller.rb
+++ photos_store_app/app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            # deliver_later uses background job to send email
+            # makes use of rails active job - quick app response unlike deliver_now
+            UserMailer.welcome_email(@user).deliver_later
             render json: { message: Messages::SUCCESS[:user_created] , email: @user.email}, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
